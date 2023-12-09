5e19abc8f5bb0c29cea61a2d8bb08e3c87a1f2ea
Simon Bar <simon.b@colisweb.com>
2023-12-09 11:40:55 +0100

    cleanup2

--- src/main/scala/Day9.scala
+++ src/main/scala/Day9.scala
@@ -8,7 +8,7 @@ object Day9 {
   def main(args: Array[String]): Unit = {
     val source = Source.fromResource("day9/input.txt")
     val input  = source.getLines()
-    val result = part2(input.toList)
+    val result = part1(input.toList)
 
     println(result)
 
@@ -17,18 +17,18 @@ object Day9 {
 
   def part1(rows: List[String]): Int =
     parse(rows).sumBy { row =>
-      step(row, List(row), _ + _)
+      step(row, List(row), _ + _, _.last)
     }
 
   def part2(rows: List[String]): Int =
     parse(rows).sumBy { row =>
-      step(row, List(row), _ - _)
+      step(row, List(row), _ - _, _.head)
     }
 
   @tailrec
-  def step(currentRow: List[Int], rowHistory: List[List[Int]], op: (Int, Int) => Int): Int =
+  def step(currentRow: List[Int], rowHistory: List[List[Int]], op: (Int, Int) => Int, elem: List[Int] => Int): Int =
     if (currentRow.forall(_ == 0)) {
-      rowHistory.tail.foldLeft(0) { case (n, row) => op(row.last, n) }
+      rowHistory.tail.foldLeft(0) { case (n, row) => op(elem(row), n) }
     } else {
       val newRow =
         currentRow
@@ -36,7 +36,7 @@ object Day9 {
           .collect { case List(first, second) => second - first }
           .toList
 
-      step(newRow, newRow :: rowHistory, op)
+      step(newRow, newRow :: rowHistory, op, elem)
     }
 
   def parse(rows: List[String]): List[List[Int]] =
